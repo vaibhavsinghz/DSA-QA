2a24d1e9c6490e5f21872a71e080c094d4982ac8
Vaibhav Kr Singh <95747953+vaibhavsinghz@users.noreply.github.com>
2023-04-26 23:02:51 +0530

    Time: 11 ms (82.80%), Space: 9.3 MB (60.16%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    int dp[21][2002];
     int solve(int i, int crSum, int target, vector<int>& nums){
         if(i == 0 and crSum == target) return 1;
         if(i <= 0) return 0;
+        if(dp[i][1001 + crSum] != -1) return dp[i][1001 + crSum];
         int plus = solve(i - 1, crSum + nums[i - 1], target, nums);
         int minus = solve(i - 1, crSum - nums[i - 1], target, nums);
-        return plus + minus;
+        return dp[i][1001 + crSum] = plus + minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
+        memset(dp, -1, sizeof dp);
         int n = nums.size();
         return solve(n, 0, target, nums);
     }
