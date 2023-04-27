91ff31e708fd7319fc0e7f7751d8d7ca30e73dcb
Vaibhav Kr Singh <95747953+vaibhavsinghz@users.noreply.github.com>
2023-04-27 15:54:15 +0530

    Time: 423 ms (34.27%), Space: 143.4 MB (16.87%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -6,8 +6,8 @@ public:
         int r = matrix.size();
         int c = matrix[0].size();
         
-        for(int i = 1; i <= c; i++) dp[1][i] = matrix[0][i-1] + dp[1][i-1];
-        for(int i = 1; i <= r; i++) dp[i][1] = matrix[i -1][0] + dp[i - 1][1];
+        for(int i = 1; i <= c; i++) dp[1][i] = matrix[0][i-1] + dp[1][i-1]; // prefix for 1st row
+        for(int i = 1; i <= r; i++) dp[i][1] = matrix[i -1][0] + dp[i - 1][1]; // prefix for 1st col
         
         for(int i = 1; i <= r; i++){
             for(int j = 1; j <= c; j++){
@@ -21,6 +21,11 @@ public:
         r1++, r2++, c1++, c2++;
         
         return (dp[r2][c2] - dp[r2][c1 - 1] - dp[r1 - 1][c2] + dp[r1 - 1][c1 - 1]);
+        /**
+        dp[r1 - 1][c1 - 1] is getting subtracted 2 times 
+        from dp[r2][c1 - 1] and dp[r1 - 1][c2]
+        so we are adding it once to balance it
+        */
     }
 };
 
