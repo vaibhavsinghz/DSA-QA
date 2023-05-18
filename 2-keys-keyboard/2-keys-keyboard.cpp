c2ba4ba9fa58e526c8b8bd0608fddc32011da811
Vaibhav Kr Singh <95747953+vaibhavsinghz@users.noreply.github.com>
2023-05-18 23:13:06 +0530

    Time: 23 ms (40.56%) | Memory: 10.4 MB (24.67%) - LeetSync

--- 2-keys-keyboard/2-keys-keyboard.cpp
+++ 2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    int dp[1001][1001];
     int helper(int n, int ans, int clip){
         if(ans > n) return 1001;
         if(ans == n) return 0;
+        if(dp[ans][clip] != -1) return dp[ans][clip];
         int copy = ans != clip ? helper(n, ans, ans) : 1001;
         int paste = clip != 0 ? helper(n, ans + clip, clip) : 1001; 
-        return min(copy, paste) + 1;
+        return dp[ans][clip] = min(copy, paste) + 1;
     }
     int minSteps(int n) {
+        memset(dp, -1, sizeof dp);
         return helper(n, 1, 0);
     }
 };
